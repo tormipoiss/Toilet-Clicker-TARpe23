c72685667787fccf7a5f040c7bdce390d2b0570d
tormipoiss <tormipoiss@gmail.com>
2024-11-20 15:04:46 +0200

    Added methods for addpasswords view, added the view itself aswell

--- Toilet Clicker.Core/obj/Debug/net8.0/Toilet Clicker.Core.AssemblyInfo.cs
+++ Toilet Clicker.Core/obj/Debug/net8.0/Toilet Clicker.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toilet Clicker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb75f8cbde732e0879163a624d66db99b1abb93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1cedefcc8dea9013b3db795ee8c8588a705338")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toilet Clicker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toilet Clicker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Toilet Clicker/Controllers/AccountsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Toilet_Clicker.Core.Domain;
+using Toilet_Clicker.Data;
+using Toilet_Clicker.Models.Accounts;
+
+namespace Toilet_Clicker.Controllers
+{
+	public class AccountsController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly ToiletClickerContext _context;
+
+		public AccountsController
+			(UserManager<ApplicationUser> userManager,
+			SignInManager<ApplicationUser> signInManager,
+			ToiletClickerContext context
+			)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> AddPassword()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			var userHasPassword = await _userManager.HasPasswordAsync(user);
+			if ( userHasPassword )
+			{
+				RedirectToAction("ChangePassword");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.GetUserAsync(User);
+				var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+				if (!result.Succeeded)
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+					return View();
+				}
+				await _signInManager.RefreshSignInAsync(user);
+				return View("AddPasswordConfirmation");
+			}
+			return View(model);
+		}
+	}
+}
