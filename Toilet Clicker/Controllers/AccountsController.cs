0cfe8b97c4a0bac2e381e59915170e96d4512096
tormipoiss <tormipoiss@gmail.com>
2024-11-27 14:33:24 +0200

    added forgot password views, methods, began on login

--- Toilet Clicker/Controllers/AccountsController.cs
+++ Toilet Clicker/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Toilet_Clicker.Core.Domain;
 using Toilet_Clicker.Data;
@@ -52,6 +53,65 @@ namespace Toilet_Clicker.Controllers
 				}
 				await _signInManager.RefreshSignInAsync(user);
 				return View("AddPasswordConfirmation");
+			}
+			return View(model);
+		}
+
+		[HttpGet]
+		public IActionResult ChangePassword()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.GetUserAsync(User);
+				if (user == null)
+				{
+					return RedirectToAction("Login");
+				}
+				var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+				if (!result.Succeeded)
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+					return View();
+				}
+				await _signInManager.RefreshSignInAsync(user);
+				return View("ChangePasswordConfirmation");
+			}
+			return View(model);
+		}
+
+		[HttpGet]
+		[AllowAnonymous]
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.FindByEmailAsync(model.Email);
+
+				if (user != null && await _userManager.IsEmailConfirmedAsync(user))
+				{
+					var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+					var passwordResetLink = Url.Action("ResetPassword", "Accounts", new { email = model.Email, token = token }, Request.Scheme);
+
+					return View("ForgotPasswordConfirmation");
+				}
+				return View("ForgotPasswordConfirmation");
 			}
 			return View(model);
 		}
